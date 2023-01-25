e8af8177cd17fd0f136b49cdae84962fde54fc48
ccarenzoIC <c.carenzo@imperial.ac.uk>
2023-01-25 12:25:18 +0000

    minor change

--- modules/chest_pain/utilities/dd_generator.py
+++ modules/chest_pain/utilities/dd_generator.py
@@ -11,6 +11,7 @@ def generate_dd_pheno(phenotypes_dictionary,mappings_icd10_snomed_path):
     with the category, the subcategory and the mapping systems used.
    
     """
+    #TODO: Use API for mapping instead of static file.
     mapping_icd10_snomed = read_csv(mappings_icd10_snomed_path)
     out = {}
     aux_result = []
@@ -86,77 +87,90 @@ def generate_dd_pheno(phenotypes_dictionary,mappings_icd10_snomed_path):
         
     
 def generate_dd_opcs(opcs_dictionary, mappings_snomed_opcs_path):
-        mapping_opcs_snomed = read_csv(mappings_snomed_opcs_path)
-        out = {}
-        aux_dic_list = []
-        for k, v in opcs_dictionary.items():
-            aux_dic = {}
-            aux = DataFrame(v, columns = ["opcs_term"])
-            aux.opcs_term = aux.opcs_term.str.replace(".","",regex=True)
-            aux_merge = aux.merge(mapping_opcs_snomed[["opcs_term","snomed_concept_id"]], on = "opcs_term")
-            members_opcs = [
-            i
-            for i in aux_merge.opcs_term.drop_duplicates().values
-            ]
-            members_snomed = [
-            int(i)
-            for i in aux_merge.snomed_concept_id.drop_duplicates().values
-            ]
-            aux_dic = [{"mapping_system":"icd_10","members":members_opcs},{"mapping_system":"snomed","members":members_snomed}]
-            aux_dic_list.append(aux_dic)
-
-            out[k] = {"mapping": aux_dic_list}
     
-        dd_opcs = ""
-        for k, v in out.items():
-            line = f"{k} = {v}\n\n"
-            # replace non-ascii characters
-            line = line.encode(encoding="ascii", errors="replace").decode().replace("?", "")
-            dd_opcs += line        
+    """
+    This function is used to obtain the mapping to snomed from the OPCS codes listed at the protocol producting a dictionary
+    with the concept, the codes and the mapping systems used.
+   
+    """
+    #TODO: Use API for mapping instead of static file.
+    mapping_opcs_snomed = read_csv(mappings_snomed_opcs_path)
+    out = {}
+    aux_dic_list = []
+    for k, v in opcs_dictionary.items():
+        aux_dic = {}
+        aux = DataFrame(v, columns = ["opcs_term"])
+        aux.opcs_term = aux.opcs_term.str.replace(".","",regex=True)
+        aux_merge = aux.merge(mapping_opcs_snomed[["opcs_term","snomed_concept_id"]], on = "opcs_term")
+        members_opcs = [
+        i
+        for i in aux_merge.opcs_term.drop_duplicates().values
+        ]
+        members_snomed = [
+        int(i)
+        for i in aux_merge.snomed_concept_id.drop_duplicates().values
+        ]
+        aux_dic = [{"mapping_system":"icd_10","members":members_opcs},{"mapping_system":"snomed","members":members_snomed}]
+        aux_dic_list.append(aux_dic)
 
-        dd_opcs = format_file_contents(dd_opcs, fast=True, mode=FileMode())
-        with open("./modules/chest_pain/data/dd_opcs.py", "wt") as f:
-            f.write(dd_opcs)
+        out[k] = {"mapping": aux_dic_list}
 
-        print("Successfully created black formatted dd_opcs.py")
+    dd_opcs = ""
+    for k, v in out.items():
+        line = f"{k} = {v}\n\n"
+        # replace non-ascii characters
+        line = line.encode(encoding="ascii", errors="replace").decode().replace("?", "")
+        dd_opcs += line        
 
-        print(dd_opcs)
+    dd_opcs = format_file_contents(dd_opcs, fast=True, mode=FileMode())
+    with open("./modules/chest_pain/data/dd_opcs.py", "wt") as f:
+        f.write(dd_opcs)
+
+    print("Successfully created black formatted dd_opcs.py")
+
+    print(dd_opcs)
 
 def generate_dd_icd10(icd10_dictionary, mappings_icd10_snomed_path):
-        mapping_icd10_snomed = read_csv(mappings_icd10_snomed_path)
-        out = {}
-        aux_dic = {}
-        aux_dic_list = []
-        for k, v in icd10_dictionary.items():
-            aux = DataFrame(v, columns = ["icd10_code"])
-            aux_merge = aux.merge(mapping_icd10_snomed[["icd10_code","snomed_concept_id"]], on = "icd10_code")
-            mapping_icd10_snomed["icd10_term"] = mapping_icd10_snomed.icd10_code.apply(lambda x: x[0:3])
-            if(aux_merge.size == 0):
-                aux_merge = aux.merge(mapping_icd10_snomed[["icd10_code","snomed_concept_id", "icd10_term"]],left_on = "icd10_code", right_on = "icd10_term" )         
-                aux_merge = aux_merge.rename(columns = {"icd10_term":"icd10_code"})
-            members_icd10 = [
-            i
-            for i in aux_merge.icd10_code.drop_duplicates().values
-            ]
-            members_snomed = [
-            int(i)
-            for i in aux_merge.snomed_concept_id.drop_duplicates().values
-            ]
-            aux_dic = [{"mapping_system":"icd_10","members":members_icd10},{"mapping_system":"snomed","members":members_snomed}]
-            aux_dic_list.append(aux_dic)
-            out[k] = {"mapping": aux_dic_list}
-    
-        dd_icd10 = ""
-        for k, v in out.items():
-            line = f"{k} = {v}\n\n"
-            # replace non-ascii characters
-            line = line.encode(encoding="ascii", errors="replace").decode().replace("?", "")
-            dd_icd10 += line        
+    """
+    This function is used to obtain the mapping to snomed from the ICD10 codes listed at the protocol producting a dictionary
+    with the concept, the codes and the mapping systems used.    
+    """
 
-        dd_icd10 = format_file_contents(dd_icd10, fast=True, mode=FileMode())
-        with open("./modules/chest_pain/data/dd_icd10.py", "wt") as f:
-            f.write(dd_icd10)
+    #TODO: Use API for mapping instead of static file.
+    mapping_icd10_snomed = read_csv(mappings_icd10_snomed_path)
+    out = {}
+    aux_dic = {}
+    aux_dic_list = []
+    for k, v in icd10_dictionary.items():
+        aux = DataFrame(v, columns = ["icd10_code"])
+        aux_merge = aux.merge(mapping_icd10_snomed[["icd10_code","snomed_concept_id"]], on = "icd10_code")
+        mapping_icd10_snomed["icd10_term"] = mapping_icd10_snomed.icd10_code.apply(lambda x: x[0:3])
+        if(aux_merge.size == 0):
+            aux_merge = aux.merge(mapping_icd10_snomed[["icd10_code","snomed_concept_id", "icd10_term"]],left_on = "icd10_code", right_on = "icd10_term" )         
+            aux_merge = aux_merge.rename(columns = {"icd10_term":"icd10_code"})
+        members_icd10 = [
+        i
+        for i in aux_merge.icd10_code.drop_duplicates().values
+        ]
+        members_snomed = [
+        int(i)
+        for i in aux_merge.snomed_concept_id.drop_duplicates().values
+        ]
+        aux_dic = [{"mapping_system":"icd_10","members":members_icd10},{"mapping_system":"snomed","members":members_snomed}]
+        aux_dic_list.append(aux_dic)
+        out[k] = {"mapping": aux_dic_list}
 
-        print("Successfully created black formatted dd_icd10.py")
+    dd_icd10 = ""
+    for k, v in out.items():
+        line = f"{k} = {v}\n\n"
+        # replace non-ascii characters
+        line = line.encode(encoding="ascii", errors="replace").decode().replace("?", "")
+        dd_icd10 += line        
 
-        print(dd_icd10)
+    dd_icd10 = format_file_contents(dd_icd10, fast=True, mode=FileMode())
+    with open("./modules/chest_pain/data/dd_icd10.py", "wt") as f:
+        f.write(dd_icd10)
+
+    print("Successfully created black formatted dd_icd10.py")
+
+    print(dd_icd10)
